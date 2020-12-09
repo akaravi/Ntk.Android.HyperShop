cda5f851a31152fa2ee8579c9ca4241d831a194e
ixiflutter1313 <mj.parishani@outlook.com>
2020-12-09 18:04:37 +0330

    add bank payment

--- app/src/main/java/ntk/android/hyper/adapter/BankSelectAdapter.java
+++ app/src/main/java/ntk/android/hyper/adapter/BankSelectAdapter.java
@@ -6,102 +6,98 @@ import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
+
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
 
 import java.util.List;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
 import es.dmoral.toasty.Toasty;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
 import ntk.android.base.NTKApplication;
-import ntk.android.base.adapter.BaseRecyclerAdapter;
 import ntk.android.base.config.NtkObserver;
 import ntk.android.base.dtomodel.bankpayment.BankPaymentOnlineTransactionModel;
 import ntk.android.base.dtomodel.hypershop.HyperShopOrderPaymentDtoModel;
 import ntk.android.base.entitymodel.bankpayment.BankPaymentPrivateSiteConfigModel;
 import ntk.android.base.entitymodel.base.ErrorException;
 import ntk.android.base.services.hypershop.HyperShopOrderService;
-import ntk.android.base.utill.FontManager;
 import ntk.android.hyper.R;
 import ntk.android.hyper.activity.hyper.CheckPaymentActivity;
 
-public class BankSelectAdapter extends BaseRecyclerAdapter<BankPaymentPrivateSiteConfigModel, BankSelectAdapter.ViewHolder> {
+public class BankSelectAdapter extends ArrayAdapter<BankPaymentPrivateSiteConfigModel> {
 
-    Long orderId;
+
     private final Context context;
+    List<BankPaymentPrivateSiteConfigModel> list;
 
-    public BankSelectAdapter(Context context, Long orderId, List<BankPaymentPrivateSiteConfigModel> arrayList) {
-        super(arrayList);
-        drawable = R.drawable.pdf;
-        this.orderId = orderId;
+    public BankSelectAdapter(Context context,  List<BankPaymentPrivateSiteConfigModel> arrayList) {
+        super(context, R.layout.row_bank_payment, arrayList);
+
         this.context = context;
+        list = arrayList;
     }
 
     @Override
-    public BankSelectAdapter.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
-        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.row_bank_payment, viewGroup, false);
-        return new BankSelectAdapter.ViewHolder(view);
+    public BankPaymentPrivateSiteConfigModel getItem(int position) {
+        return list.get(position);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        holder.LblTitle.setText(getItem(position).Title);
-        loadImage(getItem(position).LinkModuleFileLogoIdSrc, holder.Img);
-        holder.itemView.setOnClickListener(view -> {
-
-
-            HyperShopOrderPaymentDtoModel req = new HyperShopOrderPaymentDtoModel();
-            req.LinkOrderId = orderId;
-            CheckPaymentActivity.Last_Order_Id = orderId;
-            req.BankPaymentPrivateId = list.get(position).Id;
-            req.LastUrlAddressInUse = "oco.ir/" + NTKApplication.get().getApplicationParameter().APPLICATION_ID();
-            new HyperShopOrderService(view.getContext()).orderPayment(req)
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribeOn(Schedulers.io()).subscribe(new NtkObserver<ErrorException<BankPaymentOnlineTransactionModel>>() {
-                @Override
-                public void onNext(@io.reactivex.annotations.NonNull ErrorException<BankPaymentOnlineTransactionModel> response) {
-                    if (response.IsSuccess) {
-                        if (response.Item.LastUrlAddressInUse != null) {
-                            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(response.Item.LastUrlAddressInUse));
-                            context.startActivity(browserIntent);
-                        } else {
-                            Toasty.error(context, "وبگاه پرداخت فعلا در دسترس نیست لطفا بعدا تلاش فرمایید " +
-                                    "").show();
-                        }
-                    } else {
-                        Toasty.error(context, response.ErrorMessage).show();
-
-                    }
-                }
-
-                @Override
-                public void onError(@io.reactivex.annotations.NonNull Throwable e) {
-                    Toasty.error(context, "خطای سامانه").show();
-                }
-            });
-        });
+    public int getCount() {
+        return list.size();
     }
 
-
-    public class ViewHolder extends RecyclerView.ViewHolder {
-
-        @BindView(R.id.textview)
-        TextView LblTitle;
-
-        @BindView(R.id.imageview)
-        ImageView Img;
-
-
-        public ViewHolder(View view) {
-            super(view);
-            ButterKnife.bind(this, view);
-            LblTitle.setTypeface(FontManager.GetTypeface(context, FontManager.IranSans));
+    @NonNull
+    @Override
+    public View getView(int position, View view, @NonNull ViewGroup parent) {
+        if (view == null) {
+            view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.row_bank_payment, parent, false);
         }
+        ((TextView) view.findViewById(R.id.textview)).setText(getItem(position).Title);
+
+        DisplayImageOptions options = new DisplayImageOptions.Builder()
+                .showImageOnFail(R.drawable.payment_type).cacheOnDisk(true).build();
+        ImageLoader.getInstance().displayImage(getItem(position).LinkModuleFileLogoIdSrc, (ImageView) view.findViewById((R.id.imageview)), options);
+        return view;
     }
 }
+//    @Override
+//    public BankSelectAdapter.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
+//        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.row_bank_payment, viewGroup, false);
+//        return new BankSelectAdapter.ViewHolder(view);
+//    }
+//
+//    @Override
+//    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+//        holder.LblTitle.setText(getItem(position).Title);
+//        loadImage(getItem(position).LinkModuleFileLogoIdSrc, holder.Img);
+//        holder.itemView.setOnClickListener(view -> {
+//
+//
+//
+//    }
+//
+//
+//    public class ViewHolder extends RecyclerView.ViewHolder {
+//
+//        @BindView(R.id.textview)
+//        TextView LblTitle;
+//
+//        @BindView(R.id.imageview)
+//        ImageView Img;
+//
+//
+//        public ViewHolder(View view) {
+//            super(view);
+//            ButterKnife.bind(this, view);
+//            LblTitle.setTypeface(FontManager.GetTypeface(context, FontManager.IranSans));
+//        }
+//    }
+//}
--- app/src/main/java/ntk/android/hyper/fragment/BankPaymentListFragment.java
+++ app/src/main/java/ntk/android/hyper/fragment/BankPaymentListFragment.java
@@ -1,45 +1,119 @@
 package ntk.android.hyper.fragment;
 
-import androidx.recyclerview.widget.RecyclerView;
+import android.content.Intent;
+import android.net.Uri;
+import android.widget.AutoCompleteTextView;
 
-import io.reactivex.Observable;
-import java9.util.function.Function;
+import java.util.List;
+
+import es.dmoral.toasty.Toasty;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.schedulers.Schedulers;
 import ntk.android.base.Extras;
+import ntk.android.base.NTKApplication;
+import ntk.android.base.config.NtkObserver;
+import ntk.android.base.dtomodel.bankpayment.BankPaymentOnlineTransactionModel;
+import ntk.android.base.dtomodel.hypershop.HyperShopOrderPaymentDtoModel;
 import ntk.android.base.entitymodel.bankpayment.BankPaymentPrivateSiteConfigModel;
 import ntk.android.base.entitymodel.base.ErrorException;
 import ntk.android.base.entitymodel.base.FilterDataModel;
-import ntk.android.base.fragment.abstraction.AbstractionListFragment;
+import ntk.android.base.fragment.BaseFragment;
 import ntk.android.base.services.bankpayment.BankPaymentPrivateSiteConfigService;
+import ntk.android.base.services.hypershop.HyperShopOrderService;
+import ntk.android.hyper.R;
+import ntk.android.hyper.activity.hyper.CheckPaymentActivity;
 import ntk.android.hyper.adapter.BankSelectAdapter;
 
-public class BankPaymentListFragment extends AbstractionListFragment<BankPaymentPrivateSiteConfigModel> {
+public class BankPaymentListFragment extends BaseFragment {
     Long OrderId;
+    long BankId;
+
+    @Override
+    public void onCreateFragment() {
+        setContentView(R.layout.hypershop_bankpayment_fragment);
+    }
 
     @Override
     public void onCreated() {
         super.onCreated();
         assert getArguments() != null;
         OrderId = getArguments().getLong(Extras.EXTRA_FIRST_ARG, 0);
+        FilterDataModel filterDataModel = new FilterDataModel();
+        filterDataModel.RowPerPage = 20;
+        switcher.showProgressView();
+        new BankPaymentPrivateSiteConfigService(getContext()).getAll(filterDataModel)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe(new NtkObserver<ErrorException<BankPaymentPrivateSiteConfigModel>>() {
+                    @Override
+                    public void onNext(@NonNull ErrorException<BankPaymentPrivateSiteConfigModel> banks) {
+                        switcher.showContentView();
+                        if (banks.IsSuccess)
+                            bindView(banks.ListItems);
+                        else
+                            Toasty.error(getContext(), banks.ErrorMessage).show();
+                    }
 
+                    @Override
+                    public void onError(@NonNull Throwable e) {
+                        Toasty.error(getContext(), e.toString()).show();
+
+                    }
+                });
     }
 
-    @Override
-    public Function<FilterDataModel, Observable<ErrorException<BankPaymentPrivateSiteConfigModel>>> getService() {
-        return new BankPaymentPrivateSiteConfigService(getContext())::getAll;
+    private void bindView(List<BankPaymentPrivateSiteConfigModel> listItems) {
+
+
+        AutoCompleteTextView paymentType = (AutoCompleteTextView) findViewById(R.id.etPaymentBank);
+        paymentType.setAdapter(new BankSelectAdapter(getContext(), listItems));
+        paymentType.setOnItemClickListener((adapterView, view12, i, l) -> {
+            BankId = ((BankPaymentPrivateSiteConfigModel) adapterView.getItemAtPosition(i)).Id;
+            if (i >= 0)
+                paymentType.setText(((BankPaymentPrivateSiteConfigModel) adapterView.getItemAtPosition(i)).Title);
+            else
+                paymentType.setText("");
+        });
+        findViewById(R.id.btnSubmit).setOnClickListener(view -> {
+            if (BankId <= 0)
+                Toasty.error(getContext(), "لطفا یک درگاه را انتخاب نمایید").show();
+            else
+                callPayment();
+        });
     }
 
-    @Override
-    public boolean withToolbar() {
-        return false;
+    private void callPayment() {
+        HyperShopOrderPaymentDtoModel req = new HyperShopOrderPaymentDtoModel();
+        req.LinkOrderId = OrderId;
+        CheckPaymentActivity.Last_Order_Id = OrderId;
+        req.BankPaymentPrivateId = BankId;
+        req.LastUrlAddressInUse = "oco.ir/" + NTKApplication.get().getApplicationParameter().APPLICATION_ID();
+        new HyperShopOrderService(getContext()).orderPayment(req)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io()).subscribe(new NtkObserver<ErrorException<BankPaymentOnlineTransactionModel>>() {
+            @Override
+            public void onNext(@io.reactivex.annotations.NonNull ErrorException<BankPaymentOnlineTransactionModel> response) {
+                if (response.IsSuccess) {
+                    if (response.Item.LastUrlAddressInUse != null) {
+                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(response.Item.LastUrlAddressInUse));
+                        getContext().startActivity(browserIntent);
+                    } else {
+                        Toasty.error(getContext(), "وبگاه پرداخت فعلا در دسترس نیست لطفا بعدا تلاش فرمایید " +
+                                "").show();
+                    }
+                } else {
+                    Toasty.error(getContext(), response.ErrorMessage).show();
+
+                }
+            }
+
+            @Override
+            public void onError(@io.reactivex.annotations.NonNull Throwable e) {
+                Toasty.error(getContext(), "خطای سامانه").show();
+            }
+        });
     }
 
-    @Override
-    public RecyclerView.Adapter createAdapter() {
-        return new BankSelectAdapter(getContext(), OrderId, models);
-    }
 
-    @Override
-    public void ClickSearch() {
-
-    }
 }
